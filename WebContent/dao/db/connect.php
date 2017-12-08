8e5704f2eeeeab7e7b6b8823c9b2aa8cb8f67c2f
milak <milak@youwontknow.com>
2017-12-08 16:51:14 +0100

    add

--- WebContent/dao/db/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$configurationFile = file_get_contents("/home/graf/configuration.json");
-$configuration = json_decode($configurationFile);
-// Connection à la BDD
-$db = new mysqli($configuration->db->host, $configuration->db->user, $configuration->db->password, $configuration->db->instance);
-if (!$db) {
-   displayErrorAndDie('Impossible de se connecter : ' . $db->connect_error);
-}
-?>
