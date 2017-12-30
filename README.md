2e94816351cbe52f3aba68c26296e6b2651ba52b
milak <milak@youwontknow.com>
2017-12-30 23:03:12 +0100

    Prendre en compte le module itop dans la configuration et la documentation.

--- README.md
+++ README.md
@@ -19,19 +19,40 @@ For more information about the data model see mpd.html file
 
  - write the configuration file /home/graf/configuration.json :
 
+For standalone usage
 ```json
 {
+	"dao" : "db",
 	"db" : {
-		"host"     : "host",
-		"user"     : "user",
-		"password" : "password",
-		"instance" : "instance"
+		"host"     : "mydb.com",
+		"user"     : "john",
+		"password" : "mypassord",
+		"instance" : "graf"
+	}
+}
+```
+For ITOP based usage
+```json
+{
+	"dao" : "itop",
+	"itop" : {
+		"url"     		: "http://myitop.com/webservices/rest.php",
+		"user"     		: "john",
+		"password"     	: "mypassord",
+		"organisation" 	: "myenterprise",
+		"version" 		: "1.3"
 	}
 }
 ```
 
 ### Or use as docker image :
+For standalone usage
 ```shell
 docker pull milak/graf
-docker run -e GRAF_DB_USER=_user_ -e GRAF_DB_PASSWORD=_password_ -e GRAF_DB_INSTANCE=_instancename_ -e GRAF_DB_HOST=_db host_ milak/graf
+docker run -e DAO=db -e DB_USER=_user_ -e DB_PASSWORD=_password_ -e DB_INSTANCE=_instancename_ -e DB_HOST=_db host_ milak/graf
+```
+For ITOP based usage
+```shell
+docker pull milak/graf
+docker run -e DAO=itop -e ITOP_USER=_user_ -e ITOP_PASSWORD=_password_ -e ITOP_URL=_url_ -e ITOP_VERSION=_version_ -e ITOP_ORGANISATION=_organisation_ milak/graf
 ```
--- scripts/build_configuration.sh
+++ scripts/build_configuration.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-echo "{"                                           > /home/graf/configuration.json
-echo "  'db' : {"                                  >> /home/graf/configuration.json
-echo "  		'host'     : '$GRAF_DB_HOST',"     >> /home/graf/configuration.json
-echo "  		'user'     : '$GRAF_DB_USER',"     >> /home/graf/configuration.json
-echo "  		'password' : '$GRAF_DB_PASSWORD'," >> /home/graf/configuration.json
-echo "  		'instance' : '$GRAF_DB_INSTANCE'," >> /home/graf/configuration.json
-echo "   }"                                        >> /home/graf/configuration.json
-echo "}"                                           >> /home/graf/configuration.json
+if [ $DAO = "db" ]
+then
+	echo "{"                                	      			> /home/graf/configuration.json
+	echo "  'dao' : 'db',               						>> /home/graf/configuration.json
+	echo "  'db'  : {"                 		        			>> /home/graf/configuration.json
+	echo "  		'host'     		: '$DB_HOST',"   			>> /home/graf/configuration.json
+	echo "  		'user'     		: '$DB_USER',"   			>> /home/graf/configuration.json
+	echo "  		'password' 		: '$DB_PASSWORD',"			>> /home/graf/configuration.json
+	echo "  		'instance' 		: '$DB_INSTANCE'"			>> /home/graf/configuration.json
+	echo "   }"                                  	 			>> /home/graf/configuration.json
+	echo "}"                                      				>> /home/graf/configuration.json
+elif [ $DAO = "itop" ]
+	echo "{"                                      				> /home/graf/configuration.json
+	echo "  'dao'  : 'itop',                      				>> /home/graf/configuration.json
+	echo "  'itop' : {"                           				>> /home/graf/configuration.json
+	echo "  		'url'      		: '$ITOP_URL',"    			>> /home/graf/configuration.json
+	echo "  		'user'     		: '$ITOP_USER',"   			>> /home/graf/configuration.json
+	echo "  		'password' 		: '$ITOP_PASSWORD'," 		>> /home/graf/configuration.json
+	echo "  		'version'  		: '$ITOP_VERSION'," 		>> /home/graf/configuration.json
+	echo "  		'organisation'  : '$ITOP_ORGANISATION'"		>> /home/graf/configuration.json
+	echo "   }"                                   				>> /home/graf/configuration.json
+	echo "}"                                      				>> /home/graf/configuration.json
+else
+	echo "ERROR : Mode non reconnu"
+fi
