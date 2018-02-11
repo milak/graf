a9607c28ab1ba895115c991f9488fa010959081a
milak <milak@youwontknow.com>
2018-02-11 23:28:09 +0100

    Light changes in util.js

--- WebContent/vendor/graf/util.js
+++ WebContent/vendor/graf/util.js
@@ -51,89 +51,91 @@ function hidePopup(){
 function svgElementDblClicked(what,id){
 	alert("svgElementDblClicked()");
 }
-function svgElementClicked(what,id){
-	if (what == "process"){
-		showProcessContext(id);
-	} else if (what == "domain"){
-		showDomainContext(id);
-	} else if (what == "step"){
-		showProcessStepContext(id);
-	} else if (what == "tosca_item"){
-		showToscaItemContext(id);
-	} else if (what == "box"){
-		// nothing to do at now
-	} else if (what == "instance"){
-		$.getJSON( "api/service_instance.php?id="+id, function(result) {
-			var instance = result.instances[0];
-			var html = "<p>Instance</p>";
-			html += "<b>Nom</b> : "+instance.name+"<br/><br/>";
-			html += "<b>Environnement</b> : "+instance.environment.name+"<br/><br/>";
-			html += "<hr/>";
-			html+="<button onclick='hidePopup();displayServiceInstance(\""+id+"\")'><img src='images/63.png'/> ouvrir</button>";
-			html += " <button onclick='hidePopup();deleteServiceInstance(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
-			html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
-			showPopup("Détail",html);
-		}).fail(function(jxqr,textStatus,error) {
-			showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
-		});
-	} else if (what == "actor"){
-		$.getJSON( "api/element.php?id="+id, function(result) {
-			var element = result.elements[0];
-			var html = "<p>Acteur</p>";
-			html += "<b>Nom</b> : "+element.name+"<br/><br/>";
-			html += "<b>Classe</b> : "+element.class.name+"<br/><br/>";
-			html += "<hr/>";
-			html += " <button onclick='hidePopup();displayBusiness(\""+id+"\")'><img src='images/63.png'/> ouvrir</button>";
-			if (currentItem != null){
-				html += " <button onclick='hidePopup();removeItem(\""+currentItem.id+"\",\""+id+"\")'><img src='images/14.png'/> retirer</button>";
-			}
-			html += " <button onclick='hidePopup();deleteItem(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
-			html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
-			showPopup("Détail",html);
-		}).fail(function(jxqr,textStatus,error) {
-			showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
-		});
-	} else if (what == "data"){
-		$.getJSON( "api/element.php?id="+id, function(result) {
-			var element = result.elements[0];
-			var html = "<p>Donnée</p>";
-			html += "<b>Nom</b> : "+element.name+"<br/><br/>";
-			html += "<b>Classe</b> : "+element.class.name+"<br/><br/>";
-			html += "<hr/>";
-			//html += " <button onclick='hidePopup();deleteItem(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
-			if (currentItem != null){
-				html += " <button onclick='hidePopup();removeItem(\""+currentItem.id+"\",\""+id+"\")'><img src='images/14.png'/> retirer</button>";
-			}
-			html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
-			showPopup("Détail",html);
-		}).fail(function(jxqr,textStatus,error) {
-			showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
-		});
-	} else if (what == "service"){
-		$.getJSON( "api/service.php?id="+id, function(result) {
-			var service = result.services[0];
-			var html = "<p>Service</p>";
-			html += "<b>Nom</b> : "+service.name+"<br/><br/>";
-			html += "<b>Code</b> : "+service.code+"<br/><br/>";
-			html += "<hr/>";
-			html += " <button onclick='hidePopup();displayService(\""+id+"\")'><img src='images/63.png'/> ouvrir</button>";
-			if (currentItem != null){
-				html += " <button onclick='hidePopup();removeItem(\""+currentItem.id+"\",\""+id+"\")'><img src='images/14.png'/> retirer</button>";
-			}
-			html += " <button onclick='hidePopup();deleteItem(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
-			html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
-			showPopup("Détail",html);
-		}).fail(function(jxqr,textStatus,error) {
-			showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
-		});
-	} else if (what == "solution"){
-		showItemContext(id);
-	} else if ((what == "device") || (what == "server") || (what == "software")){
-		showItemContext(id);
-	} else if (what == "component"){
-		showComponentContext(id);
-	} else {
-		alert("An "+what+" of id "+id +" was clicked");
+function svgElementClicked(what,id,button){
+	if (button == 2){
+		if (what == "process"){
+			showProcessContext(id);
+		} else if (what == "domain"){
+			showDomainContext(id);
+		} else if (what == "step"){
+			showProcessStepContext(id);
+		} else if (what == "tosca_item"){
+			showToscaItemContext(id);
+		} else if (what == "box"){
+			// nothing to do at now
+		} else if (what == "instance"){
+			$.getJSON( "api/service_instance.php?id="+id, function(result) {
+				var instance = result.instances[0];
+				var html = "<p>Instance</p>";
+				html += "<b>Nom</b> : "+instance.name+"<br/><br/>";
+				html += "<b>Environnement</b> : "+instance.environment.name+"<br/><br/>";
+				html += "<hr/>";
+				html+="<button onclick='hidePopup();displayServiceInstance(\""+id+"\")'><img src='images/63.png'/> ouvrir</button>";
+				html += " <button onclick='hidePopup();deleteServiceInstance(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
+				html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
+				showPopup("Détail",html);
+			}).fail(function(jxqr,textStatus,error) {
+				showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
+			});
+		} else if (what == "actor"){
+			$.getJSON( "api/element.php?id="+id, function(result) {
+				var element = result.elements[0];
+				var html = "<p>Acteur</p>";
+				html += "<b>Nom</b> : "+element.name+"<br/><br/>";
+				html += "<b>Classe</b> : "+element.class.name+"<br/><br/>";
+				html += "<hr/>";
+				html += " <button onclick='hidePopup();displayBusiness(\""+id+"\")'><img src='images/63.png'/> ouvrir</button>";
+				if (currentItem != null){
+					html += " <button onclick='hidePopup();removeItem(\""+currentItem.id+"\",\""+id+"\")'><img src='images/14.png'/> retirer</button>";
+				}
+				html += " <button onclick='hidePopup();deleteItem(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
+				html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
+				showPopup("Détail",html);
+			}).fail(function(jxqr,textStatus,error) {
+				showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
+			});
+		} else if (what == "data"){
+			$.getJSON( "api/element.php?id="+id, function(result) {
+				var element = result.elements[0];
+				var html = "<p>Donnée</p>";
+				html += "<b>Nom</b> : "+element.name+"<br/><br/>";
+				html += "<b>Classe</b> : "+element.class.name+"<br/><br/>";
+				html += "<hr/>";
+				//html += " <button onclick='hidePopup();deleteItem(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
+				if (currentItem != null){
+					html += " <button onclick='hidePopup();removeItem(\""+currentItem.id+"\",\""+id+"\")'><img src='images/14.png'/> retirer</button>";
+				}
+				html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
+				showPopup("Détail",html);
+			}).fail(function(jxqr,textStatus,error) {
+				showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
+			});
+		} else if (what == "service"){
+			$.getJSON( "api/service.php?id="+id, function(result) {
+				var service = result.services[0];
+				var html = "<p>Service</p>";
+				html += "<b>Nom</b> : "+service.name+"<br/><br/>";
+				html += "<b>Code</b> : "+service.code+"<br/><br/>";
+				html += "<hr/>";
+				html += " <button onclick='hidePopup();displayService(\""+id+"\")'><img src='images/63.png'/> ouvrir</button>";
+				if (currentItem != null){
+					html += " <button onclick='hidePopup();removeItem(\""+currentItem.id+"\",\""+id+"\")'><img src='images/14.png'/> retirer</button>";
+				}
+				html += " <button onclick='hidePopup();deleteItem(\""+id+"\")'><img src='images/14.png'/> supprimer</button>";
+				html += " <button onclick='hidePopup()'><img src='images/33.png'/> fermer</button>";
+				showPopup("Détail",html);
+			}).fail(function(jxqr,textStatus,error) {
+				showPopup("Echec","<h1>Error</h1>"+textStatus+ " : " + error);
+			});
+		} else if (what == "solution"){
+			showItemContext(id);
+		} else if ((what == "device") || (what == "server") || (what == "software")){
+			showItemContext(id);
+		} else if (what == "component"){
+			showComponentContext(id);
+		} else {
+			alert("An "+what+" of id "+id +" was clicked");
+		}
 	}
 }
 function buildAreaList(view){
