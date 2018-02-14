433130344fb9b0602782be71b99cb516725ec9a2
milak <milak@youwontknow.com>
2018-02-14 23:51:58 +0100

    Add of new document.php api

---
+++ WebContent/api/document.php
@@ -0,0 +1,68 @@
+<?php
+require("../dao/dao.php");
+$dao->connect();
+/** METHOD GET **/
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+	if (isset($_GET["id"])){
+		$content = $dao->getDocumentContent($_GET["id"]);
+		echo $content;
+		return;
+	} else {
+		$type = '*';
+		if (isset($_GET["type"])){
+			$type = $_GET["type"];
+		}
+		if (isset($_GET["itemId"])){
+			$documents = $dao->getItemDocuments($_GET["itemId"],$type);
+        } else {
+        	// pour le moment, on ne liste pas tous les documents, on doit passer par un item
+        	die("Missing itemId argument");
+        	return;
+        }
+	}
+?>
+{ "elements" : [
+<?php
+$first = true;
+foreach ($documents as $document){
+	if ($first != true) {
+		echo ",\n";
+	}?>
+	{
+		"id"    	: "<?php echo $document->id; ?>",
+		"name" 		: "<?php echo $document->name; ?>",
+		"type" 		: "<?php echo $document->type; ?>"
+	}<?php
+	$first = false;
+}?>
+]}<?php
+/** METHOD POST **/
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST["id"])){
+        // Update
+        $id = $_POST["id"];
+        if (isset($_POST["document"])){
+            $dao->updateDocument($id,$_POST["document"]);
+        }
+    } else {
+    	if (!isset($_POST["type"])){
+    		die("Missing type argument");
+    	}
+    	$type = $_POST["type"];
+    	if (!isset($_POST["itemId"])){
+    		die("Missing itemId argument");
+    	}
+    	$itemId = $_POST["itemId"];
+    	// creation du document
+    	$document_id = $dao->createDocument("Document",$type,$_POST["document"]);
+    	$dao->addItemDocument($itemId,$document_id);
+    }
+/** METHOD DELETE **/
+} else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+	if (!isset($_GET["id"])){
+		die("Missing id argument");
+	}
+	die("Not yet implemented");
+}
+$dao->disconnect();
+?>
