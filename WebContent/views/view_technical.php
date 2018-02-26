b768aa9a41cd0141d8239b25a915bff31808a46f
milak <milak@youwontknow.com>
2018-02-26 03:46:28 +0100

    Add recursive search for technical items

--- WebContent/views/view_technical.php
+++ WebContent/views/view_technical.php
@@ -9,23 +9,58 @@ $dao->connect();
 $areas = $dao->getViewByName("technical");
 require("../svg/body.php");
 //header('Content-Type: image/svg+xml'); ne fonctionne pas car le type mime n'est pas reconnu
+$devicesUsed = array();
+$allReadyBrowsed = array();
+function recursiveSearch($id){
+	global $_allReadyBrowsed;
+	global $dao;
+	error_log('recursiveSearch'.$id);
+	if (isset($_allReadyBrowsed[$id])){
+		return array();
+	}
+	$result = array();
+	$_allReadyBrowsed[$id] = true;
+	$items = $dao->getRelatedItems($id,'*','down');
+	foreach ($items as $item){
+		if ($item->category->name == 'location'){
+			$result[] = $item;
+			break;
+		} else if ($item->category->name == 'device'){
+			$devicesUsed[] = $item;
+		} else if ($item->class->name == 'Server'){
+			$devicesUsed[] = $item;
+		}
+		foreach (recursiveSearch($item->id) as $foundItem){
+			$result[] = $foundItem;
+		}
+	}
+	return $result;
+}
 
-$items = recursiveSearch($_GET['id'],array('location'),'down');
+$items = recursiveSearch($_GET['id']);
 
-$itemById = $dao->getItems((object)['id'=>$_GET['id']]);
-foreach ($itemById as $item){
-	$itemById = $item;
-	$items[] = $item;
+foreach ($dao->getItems((object)['id'=>$_GET['id']]) as $currentItem){
+	if ($currentItem->category->name == 'location'){
+		$items[] = $currentItem;
+	} else if ($currentItem->category->name == 'device'){
+		$devicesUsed[] = $currentItem;
+	}
 	break;
 }
+// Pour chaque localisation trouvée
 foreach ($items as $item){
 	if (isset($areas[$item->name])){
-		$area = $areas[$item->name];
-		$area->elements[] = $item;
-		$item->type = $itemById->category->name;
-		$item->display->class = $itemById->category->name;
-		error_log($itemById->category->name);
+		$area 					= $areas[$item->name];
+		$area->elements[] 		= $item;
+		$item->type 			= $item->category->name;
+		$item->display 			= new stdClass();
+		$item->display->class 	= 'rect_100_100';//$item->category->name;
+		error_log($item->category->name);
 		$area->setNeeded(true);
+		$item->subElements 		= array();
+		foreach ($dao->getRelatedItems($item->id,'*','up') as $overitem){
+			$item->subElements[] = $overitem;
+		}
 	}
 }
 // Conserver uniquement les zones racines nécessaires
