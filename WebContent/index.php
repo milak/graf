0e9f7a1fa1d0a2fa4bb70f57d96ee114fe84a998
milak <milak@youwontknow.com>
2018-04-16 23:33:52 +0200

    Use of ogma for graph api

--- WebContent/index.php
+++ WebContent/index.php
@@ -109,10 +109,12 @@ html {
 	<script type="text/javascript" src="vendor/jquery-i18next/i18next.min.js"></script>
 	<script type="text/javascript" src="vendor/jquery-i18next/jquery-i18next.min.js"></script>
 	
-	<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Promise,fetch"				 type="text/javascript"></script>
+	<!--script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Promise,fetch"				 type="text/javascript"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.2.11/cytoscape.min.js" 		 type="text/javascript"></script>
     <script src="https://cdn.rawgit.com/maxkfranz/weaver/v1.2.0/dist/weaver.min.js" 			 type="text/javascript"></script>
-    <script src="https://cdn.rawgit.com/cytoscape/cytoscape.js-spread/1.3.1/cytoscape-spread.js" type="text/javascript"></script>
+    <script src="https://cdn.rawgit.com/cytoscape/cytoscape.js-spread/1.3.1/cytoscape-spread.js" type="text/javascript"></script-->
+    
+    <script type="text/javascript" src="vendor/ogma/ogma.min.js"></script>
 	
 	<script type="text/javascript" src="vendor/graf/util.js"></script>
 	<script type="text/javascript" src="vendor/graf/view.js"></script>
