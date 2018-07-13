37e5bdb5befb352c6a3d1e0f4d746533b118d39b
milak <milak@youwontknow.com>
2018-07-13 23:23:34 +0200

    Add phase and steps display

--- WebContent/vendor/graf/project.js
+++ WebContent/vendor/graf/project.js
@@ -5,10 +5,32 @@ global.project = {
 	},
 	setCurrent : function(aProject){
 		this._currentProject = aProject;
+		/** Apply currentItem change */
+		if (this._currentProject == null){
+			$("*[data-provider^='currentProject']").each(function(index,listener){
+				listener = $(listener);
+				var attribute = listener.attr("data-provider");
+				var changed = false;
+				listener.text(this._currentProject.name);
+				changed = true;
+				listener.trigger("change");
+			});
+		} else {
+			$("*[data-provider^='currentProject']").each(function(index,listener){
+            	listener = $(listener);
+            	var attribute = listener.attr("data-provider");
+            	if (attribute == "currentProject.name"){
+            		listener.text(this._currentProject.name);
+            	}
+				listener.trigger("change");
+        	});
+		}
 		this.refresh();
 	},
 	open : function(aProjectId){
-		
+		$.getJSON( "api/project.php?id="+aProjectId, function(result) {
+			global.project.setCurrent(result.objects[0]);
+		});
 	},
 	refresh : function(){
 		
